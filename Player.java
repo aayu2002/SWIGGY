ca569a9de80763b61d388436369411a749b3ddf3
aayu2002 <91870028+aayu2002@users.noreply.github.com>
2024-07-10 21:32:46 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,31 @@
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void takeDamage(int damage) {
+        this.health = Math.max(0, this.health - damage);
+    }
+}
